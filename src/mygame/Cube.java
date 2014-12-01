e2f710ce93543d16fdbf4f9f45686c02bc22051e
crisscrosskao <crisscrosskao@gmail.com>
2014-12-01 16:26:32 -0500

    creating Cube class

--- src/mygame/Cube.java
+++ src/mygame/Cube.java
@@ -4,10 +4,18 @@
  */
 package mygame;
 
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
 /**
  *
  * @author crisscrosskao
  */
-public class Cube {
+public class Cube extends Geometry {
     
+    private Box b;
+    public Cube(Vector3f loc, ) {
+        Box b = new 
+    }
 }
